7ad28c8bfcd1ca7d43d46d90288951af77e14592
lauchi103 <louisdaslauch1@gmail.com>
2024-02-18 13:03:04 +0100

    index.html fertig

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 
+from .models import Question
+
 def index(request):
-    return HttpResponse("hello, world! ich bin eric, hier ist der Poll index")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {
+        "latest_question_list":latest_question_list,
+    }
+    return render(request , "polls/index.html",context)
+
+def detail(request, question_id):
+    response = "Your looking at question %s."
+    return HttpResponse(response % question_id)
+
+def results(request,question_id):
+    response = "You're looking at the results of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request,question_id):
+    response = "You're voting on question %s."
+    return HttpResponse(response % question_id)
